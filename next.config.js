7d64d9aa90b966e1fc14709bcfad4da790de36d8
Srendi <info@denizdemiroglu.de>
2024-02-04 21:02:15 +0100

    Bump some deps, change some dev container related settings to fix minor issues and add support for jetbrains based dev containers. Fixed fast refresh for dev container runtimes

--- next.config.js
+++ next.config.js
@@ -4,6 +4,16 @@ const nextConfig = {
     images: {
         domains: ['intelligence-modding.de'],
     },
+    webpack: (config, context) => {
+        // Enable polling based on env variable being set
+        if(process.env.NEXT_WEBPACK_USEPOLLING) {
+            config.watchOptions = {
+                poll: 300,
+                aggregateTimeout: 300
+            }
+        }
+        return config
+    },
 }
 
 module.exports = nextConfig
