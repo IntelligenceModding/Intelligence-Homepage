1f122e3e24dc303c417c85224482dbbc0d8e5c80
Srendi <info@denizdemiroglu.de>
2024-03-21 22:45:38 +0100

    Fix position of footer

--- app/(overview)/layout.tsx
+++ app/(overview)/layout.tsx
@@ -7,21 +7,23 @@ import {Footer} from "@/app/ui/footer";
 const inter = Inter({subsets: ['latin']})
 
 export const metadata: Metadata = {
-    title: 'Intelligence',
+  title: 'Intelligence',
 }
 
 export default function RootLayout({
-                                       children,
+                                     children,
                                    }: {
-    children: React.ReactNode
+  children: React.ReactNode
 }) {
-    return (
-        <html className='dark' lang="en">
-        <body className={`${inter.className}`}>
-        <Navbar/>
+  return (
+      <html className='dark' lang="en">
+      <body className={`${inter.className} flex flex-col min-h-screen`}>
+      <Navbar/>
+      <div className="flex-grow dark:bg-slate-800 bg-white flex items-center justify-center">
         {children}
-        <Footer/>
-        </body>
-        </html>
-    )
+      </div>
+      <Footer/>
+      </body>
+      </html>
+  )
 }
